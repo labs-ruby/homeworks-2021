fc14f33b5de533b13bf92007901c8f8af5110294
vitalygv <kewl.dkmr@gmail.com>
2021-04-23 00:03:34 +0300

    remove ternary operator, run rubocop

--- HW02/vitaly_gavrilenya/my_methods.rb
+++ HW02/vitaly_gavrilenya/my_methods.rb
@@ -2,7 +2,7 @@ module NewArrayMethods
   refine Array do
     def my_map
       result_array = []
-      for i in 0...self.length
+      for i in 0...length
         result_array << yield(self[i])
       end
       result_array
@@ -10,14 +10,14 @@ module NewArrayMethods
 
     def my_select
       result_array = []
-      for i in 0...self.length
-        yield(self[i]) ? result_array << self[i] : result_array
+      for i in 0...length
+        result_array << self[i] if yield(self[i])
       end
       result_array
     end
 
     def my_each
-      for i in 0...self.length
+      for i in 0...length
         yield(self[i])
       end
       self
@@ -29,6 +29,6 @@ using NewArrayMethods
 
 my_ary = [1, 2, 3, 4]
 
-p my_ary.my_map{ |el| el + 1 }
-p my_ary.my_select{ |el| el > 2 }
-p my_ary.my_each{ |el| p el + 2 }
+p my_ary.my_map { |el| el + 1 }
+p my_ary.my_select { |el| el > 2 }
+p my_ary.my_each { |el| p el + 2 }
