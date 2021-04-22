5d8e9d044aef1cb10411f6614e30006efb21cb4f
uladkonop <vladknpljov@gmail.com>
2021-04-22 22:16:56 +0300

    add my_select method

--- HW02/Uladzislau_Kanapliou/hw02.rb
+++ HW02/Uladzislau_Kanapliou/hw02.rb
@@ -1,17 +1,24 @@
-module MyArrayMethods
+# frozen_string_literal: true
 
+# rubocop:disable Style/For
+module MyArrayMethods
   refine Array do
-     def my_each
-	  for i in 0..self.size-1
-	    yield self[i]
-	  end
+    def my_each
+      for i in 0...size
+        yield(self[i])
+      end
     end
 
     def my_select
+      new_array = []
+      for i in 0...size
+        new_array << self[i] if yield(self[i])
+      end
+      new_array
     end
 
-    def my_map	
+    def my_map
     end
   end
-
 end
+# rubocop:enable Style/For
