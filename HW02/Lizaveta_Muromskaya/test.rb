077a51f7bdcfe8513919d499c0d47d7f07c2b5d7
lizamuromskaya <lizavetamuromskaya@gmail.com>
2021-04-23 23:55:16 +0300

    [HW-2] Refactoring

--- HW02/Lizaveta_Muromskaya/test.rb
+++ HW02/Lizaveta_Muromskaya/test.rb
@@ -4,10 +4,10 @@ load 'my_array_methods.rb'
 using Arraymethod
 array = [1, 2, 3, 4, 5, 6]
 array.my_each { |element| p element }
-puts "========"
-array.my_map { |element|  p element + 2 }
-puts "========"
+puts '========'
+array.my_map { |element| p element + 2 }
+puts '========'
 array.my_select(&:even?)
-puts "========"
+puts '========'
 array.my_select(&:integer?)
-puts "========"
+puts '========'
