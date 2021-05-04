716abba902f65f14d1a7c1b1d1e3563b3a2622ea
uladkonop <vladknpljov@gmail.com>
2021-05-04 20:16:10 +0300

    add constants

--- HW03/Uladzislau_Kanapliou/Uladzislau_Kanapliou_hw_03_t_02.rb
+++ HW03/Uladzislau_Kanapliou/Uladzislau_Kanapliou_hw_03_t_02.rb
@@ -1,18 +1,21 @@
+# frozen_string_literal: true
+
+EXP_FIND = %r{/*.+\bPOST/*.+}.freeze
+EXP_DATE = %r{\d{2}/\w{3}/\d{4}(:\d{2}){3}..\d{4}}.freeze
+EXP_IP = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/.freeze
+EXP_PATH = %r{^/|//|(/[\w-]+)+[\s]}.freeze
+
 def task2(logs)
-  exp_find = %r{/*.+\bPOST/*.+}
-  exp_date = %r{\d{2}/\w{3}/\d{4}(:\d{2}){3}..\d{4}}
-  exp_ip = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/
-  exp_path = %r{^/|//|(/[\w-]+)+[\s]}
   return_array = []
   new_line = ''
-  if logs.match(exp_find)
+  if logs.match(EXP_FIND)
     raw_split_log_array = logs.split("\n")
     raw_split_log_array.each do |line|
-      next unless line.match(exp_find)
+      next unless line.match(EXP_FIND)
 
-      new_line += line.match(exp_date).to_s + ' FROM: '
-      new_line += line.match(exp_ip).to_s + ' TO: '
-      new_line += line.match(exp_path).to_s.upcase
+      new_line += line.match(EXP_DATE).to_s + ' FROM: '
+      new_line += line.match(EXP_IP).to_s + ' TO: '
+      new_line += line.match(EXP_PATH).to_s.upcase
       return_array << new_line
       new_line = ''
     end
