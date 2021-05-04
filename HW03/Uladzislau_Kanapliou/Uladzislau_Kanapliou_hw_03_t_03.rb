bab3ca973f1e32477329e807870860cb94b0cf01
uladkonop <vladknpljov@gmail.com>
2021-05-04 22:33:25 +0300

    try to simplify method

--- HW03/Uladzislau_Kanapliou/Uladzislau_Kanapliou_hw_03_t_03.rb
+++ HW03/Uladzislau_Kanapliou/Uladzislau_Kanapliou_hw_03_t_03.rb
@@ -1,31 +1,28 @@
+# frozen_string_literal: true
+
 require 'date'
 
+EXP_FIND = %r{/*.+\bCalling core/*.+}.freeze
+
 def task3(logs)
-  exp_find = %r{/*.+\bCalling core/*.+}
-
   return_array = []
-  datetimes = []
 
-  if logs.match(exp_find)
+  if logs.match(EXP_FIND)
     raw_split_log_array = logs.split("\n")
-    raw_split_log_array.each do |line|
-      next unless line.match(exp_find)
 
-      datetime = DateTime.parse(line)
-      datetimes << datetime
+    datetimes = raw_split_log_array.select { |line| line.match(EXP_FIND) }.map do |line|
+      DateTime.parse(line)
     end
 
-    if datetimes.size == 1
-      '0'
-    elsif datetimes.size == 2
-      ((datetimes[1].strftime('%Q').to_i -
-        datetimes[0].strftime('%Q').to_i).to_s[0...-2].to_f / 10).to_s
-    else
-      (0...datetimes.size - 1).each do |i|
-        return_array << ((datetimes[i + 1].strftime('%Q').to_i -
-                    datetimes[i].strftime('%Q').to_i).to_s[0...-2].to_f / 10).to_s
-      end
+    (0...datetimes.size - 1).each do |i|
+      return_array << ((datetimes[i + 1].strftime('%Q').to_i -
+                  datetimes[i].strftime('%Q').to_i).to_s[0...-2].to_f / 10).to_s
+    end
+
+    if !return_array.empty?
       return_array
+    else
+      '0'
     end
   else
     '0'
