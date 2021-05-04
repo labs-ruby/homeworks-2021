73fb9911c2c530a08f9f08ba11dd9ac2d521ae4a
Egor Chernooky <e.chernooky@gmail.com>
2021-05-04 17:41:16 +0300

    Removed array

--- HW03/EgorChernooky/egor_chernooky_hw_03_t_2.rb
+++ HW03/EgorChernooky/egor_chernooky_hw_03_t_2.rb
@@ -11,14 +11,12 @@ logs =
 10.6.246.101 - - [23/Apr/2018:20:31:39 +0300] POST /test/2/messages HTTP/1.1 200 48 0.0290'
 
 def task2(text)
-  output_array = []
   text.split("\n").select { |line| line.downcase.include?('post') }.map do |line|
     date = line.scan(DATE).join
     from = line.scan(FROM).join
     to = line.scan(TO).join.upcase
-    output_array << "#{date} FROM: #{from} TO: #{to}"
+    "#{date} FROM: #{from} TO: #{to}"
   end
-  output_array
 end
 
 p task2(logs)
