564230a0598a7b1a4e6e73f4880fe5af0131c809
IvanSomething <ischlovski@gmail.com>
2021-05-07 18:43:32 +0300

    Create class Notification

---
+++ HW04/ivan_shklovski/notification.rb
@@ -0,0 +1,12 @@
+class Notification
+  attr_reader :homework, :status
+
+  def initialize(homework:, status: nil)
+    @homework = homework
+    @status = status
+  end
+
+  def to_s
+    @status || 'empty notification'
+  end
+end
