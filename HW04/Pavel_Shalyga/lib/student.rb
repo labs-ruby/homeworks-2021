d2da88bbda6a5131042e0b7b21f099c6d47bfd4f
Pavel <pavel.shalyga.1992@gmail.com>
2021-05-08 20:16:29 +0300

    Added Student class

---
+++ HW04/Pavel_Shalyga/lib/student.rb
@@ -0,0 +1,30 @@
+# The Student class is used to create objects that can interact with mentor objects
+class Student
+  attr_writer :name, :surname
+  attr_accessor :notification, :homeworks, :mentor
+
+  def initialize(name, surname)
+    @name = name
+    @surname = surname
+    @notification = Notification.new
+    @homeworks = []
+  end
+
+  def notifications
+    @notification.messages
+  end
+
+  def mark_as_read!
+    @notification.messages.clear
+  end
+
+  def to_work!(homework)
+    mentor.notification.add("I'm working with #{homework}")
+    index = @homeworks.index(homework)
+    @homeworks[index].working!
+  end
+
+  def to_check!(homework)
+    mentor.notification.add("My homework: #{homework}")
+  end
+end
