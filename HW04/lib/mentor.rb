b46c3a8023768172be836de5254ee38ffa6e24a3
uladkonop <vladknpljov@gmail.com>
2021-05-08 21:04:48 +0300

    add initialize, add_homework, subscribe_to!, mark_as_read! methods

--- HW04/lib/mentor.rb
+++ HW04/lib/mentor.rb
@@ -1,11 +1,26 @@
 require_relative 'homework'
 
 class Mentor
-	attr_reader :name, :surname
+	attr_reader :name, :surname, :notifications
   def initialize(name, surname)
     @name, @surname = name, surname
+    @notifications = []
   end
   def add_homework(title, description, student)
-  	Homework.new(title, description, student)
+  	new_homework = Homework.new(title, description)
+  	student.homeworks << new_homework
+  	new_homework
+  end
+  def subscribe_to!(student)
+  	student.attach_mentor(self)
+  	p "Mentor #{@name} #{@surname} was attached to student #{student.name} #{student.surname}"
+  end
+  def mark_as_read!
+    	case @notifications.size 
+    	when 0
+    		p 'All notifications are read'
+		else
+			notifications.clear    		
+    	end
   end
 end
