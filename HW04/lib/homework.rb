928ac976ebbf149c30f8088e5e2c3680fa743b75
uladkonop <vladknpljov@gmail.com>
2021-05-08 21:06:01 +0300

    add initialize method

--- HW04/lib/homework.rb
+++ HW04/lib/homework.rb
@@ -1,6 +1,9 @@
 class Homework
-	attr_reader :title, :description, :student
-  def initialize(title, description, student)
-    @title, @description, @student = title, description, student
+	attr_reader :title, :description
+	attr_accessor :state, :answer
+  def initialize(title, description)
+    @title, @description = title, description
+    @state = {:undone => true,:done => false, :accepted => false, :in_progress => false }
+    @answer = ""
   end
 end
