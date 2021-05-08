fc1944b1700bf5a2069b8f7531de2964785ccd47
uladkonop <vladknpljov@gmail.com>
2021-05-08 21:03:33 +0300

    add initialise method, mark_as_read!, attach_mentor, to_work!, add_answer!, to_check!

--- HW04/lib/student.rb
+++ HW04/lib/student.rb
@@ -1,6 +1,33 @@
 class Student
-	attr_reader :name, :surname
+	attr_reader :name, :surname, :notifications
+	attr_accessor :homeworks
   def initialize(name, surname)
     @name, @surname = name , surname
+    @notifications = []
+    @homeworks = []
+    @mentors = []
+  end
+  def mark_as_read!
+    	case @notifications.size 
+    	when 0
+    		p 'All notifications are read'
+		else
+			notifications.clear    		
+    	end
+  end
+  def attach_mentor(mentor)
+  	@mentors << mentor
+  end
+  def to_work!(homework)
+  	homework.state[:undone] = false
+  	homework.state[:in_progress] = true
+  end
+  def add_answer!(homework, notification_message)
+  	homework.state[:done] = true
+  	homework.state[:in_progress] = false
+  	homework.answer = notification_message
+  end
+  def to_check!(homework)
+  	p "#{homework.title} done, please check"
   end
 end
