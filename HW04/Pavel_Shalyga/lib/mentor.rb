cd70e39cc9569eabe98a0994d407b0899c39c10a
Pavel <pavel.shalyga.1992@gmail.com>
2021-05-08 20:16:49 +0300

    Added Mentor class

---
+++ HW04/Pavel_Shalyga/lib/mentor.rb
@@ -0,0 +1,42 @@
+# The Mentor class is used to create objects that can interact with student objects
+class Mentor
+  attr_writer :name, :surname
+  attr_reader :student
+  attr_accessor :notification
+
+  def initialize(name, surname)
+    @name = name
+    @surname = surname
+    @notification = Notification.new
+  end
+
+  def notifications
+    @notification.messages
+  end
+
+  def mark_as_read!
+    @notification.messages.clear
+  end
+
+  def add_homework(title, description)
+    homework = Homework.new(title, description)
+    @student.homeworks << homework
+    @student.notification.add("New homework: #{title}, #{description}")
+    homework
+  end
+
+  def subscribe_to!(student)
+    @student = student
+    student.mentor = self
+  end
+
+  def reject!(homework)
+    homework.reject!
+    student.notification.add("Homework #{homework} rejected")
+  end
+
+  def accept!(homework)
+    homework.accept!
+    student.notification.add("Homework #{homework} accepted")
+  end
+end
