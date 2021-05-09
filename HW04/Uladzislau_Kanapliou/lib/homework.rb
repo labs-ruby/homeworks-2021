f23d11c43b7255e6042c2b2d921d8a5a5e2ef224
uladkonop <vladknpljov@gmail.com>
2021-05-09 21:18:51 +0300

    refactor of homework class

--- HW04/Uladzislau_Kanapliou/lib/homework.rb
+++ HW04/Uladzislau_Kanapliou/lib/homework.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
 class Homework
+  STATES = {
+    undone: 'undone',
+    done: 'done',
+    in_progress: 'in progress',
+    accepted: 'accepted'
+  }.freeze
+
   attr_reader :title, :description, :student
-  attr_accessor :state, :answer
+  attr_accessor :answer
+
   def initialize(title, description, student)
     @student = student
     @title = title
     @description = description
-    @state = { undone: true, done: false, accepted: false, in_progress: false }
     @answer = ''
+    @state = STATES[:undone]
+  end
+  
+  def change_state(state)
+    @state = STATES.fetch(state)
   end
 end
