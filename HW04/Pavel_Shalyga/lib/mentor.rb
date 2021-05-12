1c121a769ffa320a98ddeaca1c592cf2be56dfbd
Pavel <pavel.shalyga.1992@gmail.com>
2021-05-12 16:05:54 +0300

    Added the ability for the mentor to subscribe to several students

--- HW04/Pavel_Shalyga/lib/mentor.rb
+++ HW04/Pavel_Shalyga/lib/mentor.rb
@@ -1,13 +1,14 @@
 # The Mentor class is used to create objects that can interact with student objects
 class Mentor
   attr_writer :name, :surname
-  attr_reader :student
+  attr_reader :students
   attr_accessor :notification
 
   def initialize(name, surname)
     @name = name
     @surname = surname
     @notification = Notification.new
+    @students = []
   end
 
   def notifications
@@ -19,25 +20,28 @@ class Mentor
   end
 
   def add_homework(title, description)
-    homework = Homework.new(title, description)
-    @student.homeworks << homework
-    @student.notification.add("#{@name} #{@surname}: New homework: #{title}, #{description}")
-    homework
+    @students.each do |student|
+      student.homeworks << Homework.new(title, description)
+      student.notification.add("#{@name} #{@surname}: New homework: #{title}, #{description}")
+    end
   end
 
   def subscribe_to!(student)
-    @student = student
+    @students << student
     student.mentor = self
+    student.notification.add("#{@name} #{@surname}: I'm your new mentor")
   end
 
-  def reject!(homework)
+  def reject!(student, homework)
     homework.reject!
-    @student.notification.add("#{@name} #{@surname}: Homework #{homework} rejected")
+    index = @students.index(student)
+    @students[index].notification.add("#{@name} #{@surname}: Homework #{homework} rejected")
   end
 
-  def accept!(homework)
+  def accept!(student, homework)
     homework.accept!
-    @student.notification.add("#{@name} #{@surname}: Homework #{homework} accepted")
+    index = @students.index(student)
+    @students[index].notification.add("#{@name} #{@surname}: Homework #{homework} accepted")
   end
 
   def sent_to(student, message)
