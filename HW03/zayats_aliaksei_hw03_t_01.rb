e925a8e5c057210216a1c59dae796320b5ee61cc
ZayatsAliaksei <legoza@narod.ru>
2021-05-12 17:27:50 +0300

    first task with remarks

--- HW03/zayats_aliaksei_hw03_t_01.rb
+++ HW03/zayats_aliaksei_hw03_t_01.rb
@@ -1,6 +1,4 @@
 def find_error_in_text(text)
   array_text = text.split("\n")
-  array_text.each do |string|
-    puts string if string =~ /[Ee]rror/
-  end
+  array_text.each { |string| return string if string.downcase.include?('error') }
 end
