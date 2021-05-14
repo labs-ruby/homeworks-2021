47208c89d68a9e56e0ca9be099e99de1fe179b06
m3xq <z3w1l0q@protonmail.com>
2021-05-14 15:14:31 -0700

    Fix tests

--- HW02/vladislav_mitin/spec/my_enumerable_methods_spec.rb
+++ HW02/vladislav_mitin/spec/my_enumerable_methods_spec.rb
@@ -3,41 +3,45 @@ require_relative '../my_enumerable_methods'
 using MyEnumerableMethods
 
 RSpec.describe MyEnumerableMethods do
-  subject { [1, 2, 3] }
-
   describe '#my_each' do
-    context 'when method was called without block' do
-      it { expect([].my_each).to be_an Enumerator }
-      it { expect(subject.my_each).to be_an Enumerator }
+    context 'when was called without block' do
+      subject { [1, 2, 3].my_each }
+
+      it { is_expected.to be_an Enumerator }
     end
 
-    context 'when method was called with block' do
-      it { expect([].my_each { _1 + 1 }).to eq [] }
-      it { expect(subject.my_each { _1 + 1 }).to eq subject }
+    context 'when was called with block' do
+      subject { [1, 2, 3].my_each { _1 + 1 } }
+
+      it { is_expected.to eq([1, 2, 3]) }
     end
   end
 
   describe '#my_map' do
-    context 'when method was called without block' do
-      it { expect([].my_map).to be_an Enumerator }
-      it { expect(subject.my_map).to be_an Enumerator }
+    context 'when was called without block' do
+      subject { [1, 2, 3].my_map }
+
+      it { is_expected.to be_an Enumerator }
     end
 
-    context 'when method was called with block' do
-      it { expect([].my_map { _1 + 1 }).to eq [] }
-      it { expect(subject.my_map { _1 + 1 }).to eq [2, 3, 4] }
+    context 'when was called with block' do
+      subject { [1, 2, 3].my_map { _1 + 1 } }
+
+      it { is_expected.to eq([2, 3, 4]) }
     end
   end
 
   describe '#my_select' do
-    context 'when method was called without block' do
-      it { expect([].my_select).to be_an Enumerator }
-      it { expect(subject.my_select).to be_an Enumerator }
+    context 'when was called without block' do
+      subject { [1, 2, 3].my_select }
+
+      it { is_expected.to be_an Enumerator }
     end
 
-    context 'when method was called with block' do
-      it { expect([].my_select(&:even?)).to eq [] }
-      it { expect(subject.my_select(&:even?)).to eq [2] }
+    context 'when was called with block' do
+      subject { [1, 2, 3].my_select(&:even?) }
+
+      it { is_expected.to eq([2]) }
     end
   end
 end
