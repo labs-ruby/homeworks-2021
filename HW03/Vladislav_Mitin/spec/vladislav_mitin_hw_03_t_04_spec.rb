ae0c8ecb174372a6f5aec38c326804a64df61f10
m3xq <z3w1l0q@protonmail.com>
2021-05-15 01:08:39 -0700

    Fix spec for fourth task

--- HW03/Vladislav_Mitin/spec/vladislav_mitin_hw_03_t_04_spec.rb
+++ HW03/Vladislav_Mitin/spec/vladislav_mitin_hw_03_t_04_spec.rb
@@ -3,24 +3,18 @@ require_relative '../vladislav_mitin_hw_03_t_04'
 RSpec.describe 'task4' do
   subject { task4(text) }
 
-  context 'when text is not string' do
+  context 'when the method argument is not string' do
     let(:text) { 2 }
     it { within_block_is_expected.to raise_error NoMethodError }
   end
 
-  context 'when text has two digits and seven letters' do
+  context 'when the method argument has digits and letters' do
     let(:text) { 'wicked 2.. ! 3 w' }
 
     it { is_expected.to eq({ digits: 2, letters: 7 }) }
   end
 
-  context 'when text has five letters and five letters' do
-    let(:text) { 'a 123 bcd-+32e' }
-
-    it { is_expected.to eq({ digits: 5, letters: 5 }) }
-  end
-
-  context 'when text is empty' do
+  context 'when the method argument is empty' do
     let(:text) { '' }
 
     it { is_expected.to eq({ digits: 0, letters: 0 }) }
