d8cb7b4f72a3bd9b77dafcd1c07471356efbe035
uladkonop <vladknpljov@gmail.com>
2021-05-22 09:04:13 +0300

    add specs for 3.3 task

---
+++ HW03/Uladzislau_Kanapliou/Uladzislau_Kanapliou_hw_03_t_03_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require_relative './Uladzislau_Kanapliou_hw_03_t_03'
+
+describe '.task3' do
+  it 'it return an array of strings' do
+    logs = '2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - Calling core with action: event
+			2018-04-23 17:17:49.7 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+			2018-04-23 17:17:49.8 ubuntu-xenial[14319] Debug - docker event processed
+			2018-04-23 17:18:19.5 ubuntu-xenial[14319] Debug - monitoring grid communication health
+			2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages
+			2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
+			2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
+			2018-04-23 17:19:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages'
+    expect(Array).to eq(task3(logs).class)
+  end
+
+  it 'if there are no valid events at the entrance or it is the only one to return "0"' do
+    line = '2018-04-23 20:30:42'
+    expect('0').to eq(task3(line))
+  end
+end
