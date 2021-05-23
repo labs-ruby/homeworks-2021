bef2d7f2348cd526888ac5ae6f81f47ffa9a7077
uladkonop <vladknpljov@gmail.com>
2021-05-23 08:08:05 +0300

    rewrite expect expression

--- HW03/Uladzislau_Kanapliou/Uladzislau_Kanapliou_hw_03_t_04_spec.rb
+++ HW03/Uladzislau_Kanapliou/Uladzislau_Kanapliou_hw_03_t_04_spec.rb
@@ -12,6 +12,6 @@ describe '.task4' do
 			2018-04-23 17:18:38.8 ubuntu-xenial[14319] Debug - connecting to: 10.6.246.101
 			2018-04-23 17:18:59.8 ubuntu-xenial[14319] Debug - inside docker_handle_event
 			2018-04-23 17:19:38.8 ubuntu-xenial[14319] Debug - Calling core with action: messages'
-    expect({:letters=>320, :numbers=>178}).to eq(task4(logs))
+    expect(task4(logs)).to eq({ letters: 320, numbers: 178 })
   end
 end
