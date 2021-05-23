8032a454758f32a110aa8aaae353d1337abbc089
Egor Chernooky <e.chernooky@gmail.com>
2021-05-23 16:26:44 +0300

    Update my_each method and add tests for HW02

---
+++ HW05/EgorChernooky/array_methods_spec.rb
@@ -0,0 +1,117 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative '/home/egor/homeworks-2021/HW02/EgorChernooky/ArrayMethods'
+
+using ArrayMethods
+
+RSpec.describe ArrayMethods do
+  let(:array) { [1, 2, 3, 4, 5] }
+  let(:mapped_array) { [1, 4, 9, 16, 25] }
+  let(:selected_arr) { [3, 4, 5] }
+  let(:reversed_array) { [5, 4, 3, 2, 1] }
+  let(:empty_array) { [] }
+
+  describe '#my_each' do
+    context 'when array is empty' do
+      it 'should return an empty array' do
+        expect(empty_array.my_each { |item| p item}).to eq empty_array
+      end
+    end
+
+    context 'when array is not empty' do
+      it 'should return an origin array' do
+        expect(array.my_each { |item| p item }).to eq array
+      end
+    end
+  end
+
+  describe '#my_map' do
+    context 'when array is empty' do
+      it 'should return an empty array'  do
+        expect(empty_array.my_map { |item| item**2 }).to eq empty_array
+      end
+    end
+
+    context 'when array is not empty' do
+      it 'should return a changed array but origin array should not be changed' do
+        expect(array.my_map { |item| item**2 }).to eq mapped_array
+        expect(array == mapped_array).to eq false
+      end
+    end
+  end
+
+  describe '#my_map!' do
+    context 'when array is empty' do
+      it 'should return an empty array' do
+        expect(empty_array.my_map! { |item| item**2 }).to eq empty_array
+      end
+    end
+
+    context 'when array is not empty' do
+      it 'should return a changed origin array' do
+        expect(array.my_map! { |item| item**2 }).to eq mapped_array
+        expect(array == mapped_array).to eq true
+      end
+    end
+  end
+
+  describe '#my_select' do
+    context 'when array is empty' do
+      it 'should return an empty array' do
+        expect(empty_array.my_select { |item| item > 2 }).to eq empty_array
+      end
+    end
+
+    context 'when array is not empty' do
+      it 'should return a new array and not change the origin one' do
+        expect(array.my_select { |item| item > 2 }).to eq selected_arr
+        expect(array == selected_arr).to eq false
+      end
+    end
+  end
+
+  describe '#my_empty?' do
+    context 'when array is empty' do
+      it 'should return true' do
+        expect(empty_array.my_empty?).to eq true
+      end
+    end
+
+    context 'when array is not empty' do
+      it 'should return false' do
+        expect(array.my_empty?).to eq false
+      end
+    end
+  end
+
+  describe '#my_reverse' do
+    context 'when array is empty' do
+      it 'should return an empty array' do
+        expect(empty_array.my_reverse).to eq empty_array
+      end
+    end
+
+    context 'when array is not empty' do
+      it 'should return a reversed array without changing the origin one' do
+        expect(array.my_reverse).to eq reversed_array
+        expect(array == reversed_array).to eq false
+      end
+    end
+  end
+
+  describe '#my_reverse!' do
+    context 'when array is empty' do
+      it 'should return an empty array' do
+        expect(empty_array.my_reverse!).to eq empty_array
+      end
+    end
+
+    context 'when array is not empty' do
+      it 'should return a reversed origin array' do
+        expect(array.my_reverse!).to eq reversed_array
+        expect(array == reversed_array).to eq true
+      end
+    end
+  end
+end
