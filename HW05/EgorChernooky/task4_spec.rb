cc412c1db4f3b6261cef7dab01a0827433844559
Egor Chernooky <72251104+blooond@users.noreply.github.com>
2021-05-27 12:16:32 +0300

    Update task4_spec.rb

--- HW05/EgorChernooky/task4_spec.rb
+++ HW05/EgorChernooky/task4_spec.rb
@@ -4,12 +4,23 @@ require 'rspec'
 require_relative '/home/egor/homeworks-2021/HW03/EgorChernooky/egor_chernooky_hw_03_t_4'
 
 RSpec.describe 'hw_03_t_4' do
-  let(:str) { 'hel2!lo' }
-  let(:result) { { letters: 5, digits: 1 } }
   
   describe '#task4' do
-    it 'should return the number of letters and digits' do
-      expect(task4(str)).to eq result
+    context 'when string is not empty' do
+      let(:str) { 'hel2!lo' }
+      let(:result) { { letters: 5, digits: 1 } }
+      
+      it 'should return the number of letters and digits' do
+        expect(task4(str)).to eq result
+      end
+    
+    context 'when string is empty' do
+      let(:empty_string) { '' }
+      let(:default_hash) { { letters: 0, digits: 0 } }
+      
+      it 'should return 0 for letters and digits' do
+        expect(task4(empty_string).to eq default_hash
+      end
     end
   end
 end
